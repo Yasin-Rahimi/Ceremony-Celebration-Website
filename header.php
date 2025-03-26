3284ad03e4c1e5736be4662218356abc25b78c9c
Yasin Rahimi <rahimiyasin58@gmail.com>
2025-03-26 15:36:19 +0000

    Flying head bar and home page (model 1)

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="header-container">
+        <!-------- navigation bar -------->
+        <div class="header-content-holder">
+
+            <nav>
+                <ul>
+                    <li><a href="#contact">راه های ارتباطی</a></li>
+                    <li><a href="#points">مزایا</a></li>
+                    <li><a href="#idea">ایده ها</a></li>
+                    <li><a href="#nemoone">نمونه کارها</a></li>
+                    <div class="logo-name-container">
+                        <div class="logo-holder"></div>
+                    </div>
+                </ul>
+            </nav>
+
+        </div>
+        
+        <!---------- logo ---------->
+    </div> 
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="home-container">
+
+        <!-------- home page vactor -------->
+        <div class="home-section">
+            <div class="home-vactor-holder">
+            </div>
+        </div>
+
+
+
+        <!-------- home page text -------->
+        <div class="home-section">
+            <div class="home-text-holder">
+                <div class="home-inner-text-holder">
+                    <p>تشریفات خاص و رویایی در یزد با ربان</p>
+                </div>
+                <div class="home-inner-text-holder">
+                    <pre>
+،وقتی عشق، زیبایی و طراحی بی نظیر کنار هم لحظاتی جاودانه خلق میکنند
+.با ما جشن های خاص خود را به زیباترین شکل ممکن برگذار کنید
+                    </pre>
+                </div>
+                <div class="home-button-holder">
+                    <a href="https://wa.me/+989134606965">ثبت سفارش</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
